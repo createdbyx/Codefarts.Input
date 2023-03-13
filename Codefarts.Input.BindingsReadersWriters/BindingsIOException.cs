e131bd613c1a8fc8c308cf28315384dfe898429e
Dean <deanlunz@createdbyx.com>
2023-03-12 18:19:12 -0700

    strings to constants now throws exceptions no more static methods

---
+++ Codefarts.Input.BindingsReadersWriters/BindingsIOException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Codefarts.Input;
+
+public class BindingsIOException : Exception
+{
+    public BindingsIOException()
+    {
+    }
+
+    protected BindingsIOException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public BindingsIOException(string? message) : base(message)
+    {
+    }
+
+    public BindingsIOException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
--- Codefarts.Input.BindingsReadersWriters/BindingsReader.cs
+++ Codefarts.Input.BindingsReadersWriters/BindingsReader.cs
@@ -1,10 +1,16 @@
-﻿using Codefarts.Input.Interfaces;
+﻿// <copyright file="BindingsReader.cs" company="Codefarts">
+// Copyright (c) Codefarts
+// contact@codefarts.com
+// http://www.codefarts.com
+// </copyright>
+
+using System.Collections.ObjectModel;
+using Codefarts.Input.Interfaces;
 using Codefarts.Input.Models;
 
 namespace Codefarts.Input;
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Xml.Linq;
 
@@ -13,22 +19,39 @@ using System.Xml.Linq;
 /// </summary>
 public class BindingsReader
 {
+    private const string NameKey = "name";
+    private const string InputSourceKey = "inputsource";
+    private const string SourceKey = "source";
+    private const string PlayerKey = "player";
+    private const string BindingsKey = "bindings";
+    private const string BindKey = "bind";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BindingsReader"/> class.
+    /// </summary>
+    /// <param name="getInputSource">Sets the callback function for retrieving input sources.</param>
+    public BindingsReader(Func<string, IInputSource> getInputSource)
+    {
+        this.GetInputSource = getInputSource ?? throw new ArgumentNullException(nameof(getInputSource));
+        this.Bindings = new ObservableCollection<BindingData>();
+    }
+
+    private Func<string, IInputSource> GetInputSource { get; }
+
+    public ObservableCollection<BindingData> Bindings { get; }
+
     /// <summary>
     /// Reads binding data from a xml file.
     /// </summary>
     /// <param name="file">
     /// The xml file to be read.
     /// </param>
-    /// <returns>
-    /// Returns an array of <see cref="BindingData"/> types.
-    /// </returns>
-    public static BindingData[] Read(string file, Func<string, IInputSource> getInputSource)
+    public void Read(string file)
     {
-        // System.Diagnostics.Debugger.Launch();
         var fileStream = File.OpenRead(file);
         using (fileStream)
         {
-            return Read(fileStream, getInputSource);
+            this.Read(fileStream);
         }
     }
 
@@ -38,65 +61,71 @@ public class BindingsReader
     /// <param name="stream">
     /// The xml stream to be read.
     /// </param>
-    /// <returns>
-    /// Returns an array of <see cref="BindingData"/> types.
-    /// </returns>
-    public static BindingData[] Read(Stream stream, Func<string, IInputSource> getInputSource)
+    public void Read(Stream stream)
     {
         var doc = XDocument.Load(stream);
 
-        var items = new List<BindingData>();
-
-        if (doc.Root == null || doc.Root.Name.LocalName != "bindings")
+        if (doc.Root == null || doc.Root.Name.LocalName != BindingsKey)
         {
-            return items.ToArray();
+            throw new BindingsIOException($"Root node is not '{BindingsKey}'.");
         }
 
-        var bindings = doc.Root.Elements("bind");
-        var att = new Dictionary<string, string>();
-        foreach (var binding in bindings)
+        var bindingElements = doc.Root.Elements(BindKey);
+        foreach (var binding in bindingElements)
         {
             // assign default values
-            att.Add("name", string.Empty);
-            att.Add("player", 1.ToString());
-            att.Add("inputsource", string.Empty);
-            att.Add("source", string.Empty);
-
-            // grab all relevant attributes
-            var keys = new string[att.Count];
-            att.Keys.CopyTo(keys, 0);
-            foreach (var a in keys)
-            {
-                var tmp = binding.Attribute(a);
-                if (tmp != null)
-                {
-                    att[a] = tmp.Value;
-                }
-            }
+            var nameAttribute = binding.Attribute(NameKey);
+            var playerAttribute = binding.Attribute(PlayerKey);
+            var inputSourceAttribute = binding.Attribute(InputSourceKey);
+            var sourceAttribute = binding.Attribute(SourceKey);
 
             // determine if the action, inputsource and source values were specified
-            if (string.IsNullOrWhiteSpace(att["name"]) ||
-                string.IsNullOrWhiteSpace(att["inputsource"]) ||
-                string.IsNullOrWhiteSpace(att["source"]))
-            {
-                continue;
-            }
-
+            this.ValidateValueExists(NameKey, nameAttribute);
+            this.ValidateValueExists(InputSourceKey, inputSourceAttribute);
+            this.ValidateValueExists(SourceKey, sourceAttribute);
 
             // try get player index
-            int playerIndex = 0;
-            if (!string.IsNullOrWhiteSpace(att["player"]) && !int.TryParse(att["player"], out playerIndex))
+            int playerIndex = this.DefaultPlayerIndex;
+            if (playerAttribute != null && !string.IsNullOrWhiteSpace(playerAttribute.Value))
             {
-                throw new Exception("A Player value exists but could not be parsed.");
+                int.TryParse(playerAttribute.Value.Trim(), out playerIndex);
             }
 
-            var inputSource = getInputSource?.Invoke(att["inputsource"]);
-            items.Add(new BindingData(att["name"], inputSource, att["source"], playerIndex));
+            // try to fetch input source
+            IInputSource inputSourceReference;
+            try
+            {
+                inputSourceReference = this.GetInputSource(inputSourceAttribute.Value);
+                if (inputSourceReference == null)
+                {
+                    throw new BindingsIOException($"No input source reference with name '{inputSourceAttribute.Value}' could be located.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new BindingsIOException($"Exception thrown while trying to fetch input source reference.", ex);
+            }
 
-            att.Clear();
+            // add the bindings
+            this.Bindings.Add(new BindingData(nameAttribute.Value, inputSourceReference, sourceAttribute.Value, playerIndex));
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the default player index to use when reading bindings and there is no player index specified.
+    /// </summary>
+    public int DefaultPlayerIndex { get; set; }
+
+    private void ValidateValueExists(string name, XAttribute? attribute)
+    {
+        if (attribute == null)
+        {
+            throw new BindingsIOException($"'{name}' attribute not found on a binding.");
         }
 
-        // return the reference the Doc variable
-        return items.ToArray();
+        if (string.IsNullOrWhiteSpace(attribute.Value))
+        {
+            throw new BindingsIOException($"'{name}' attribute has no value.");
+        }
     }
 }
